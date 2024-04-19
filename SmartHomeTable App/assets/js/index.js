ba94ebc8b5e386e507c657724b570402be1c768a
MariFromNZ <91533215+MariFromNZ@users.noreply.github.com>
2024-04-19 18:17:48 +1200

    validated html&scss,deleted tryfile

--- SmartHomeTable App/assets/js/index.js
+++ SmartHomeTable App/assets/js/index.js
@@ -1,12 +1,14 @@
+
+
 // for elements in progress registerBtn
 var registerBtn = document.getElementById('registerBtn');
 registerBtn.addEventListener('click', function() {
     alert('Sorry, still in development');
 });
 
-// for elements in progress
+// for login
 var loginBtn = document.getElementById('loginBtn');
-loginBtn.addEventListener('click', function(e) {
+loginBtn.addEventListener('click', function() {
     var userNameInput = document.getElementById('name').value;
     var passwordInput = document.getElementById('password').value;
     if(userNameInput === 'Mariia' && passwordInput === '123')
@@ -17,3 +19,4 @@ loginBtn.addEventListener('click', function(e) {
         alert('Username or password is not correct. Try username - Mariia, password - 123')
     }      
 });
+
